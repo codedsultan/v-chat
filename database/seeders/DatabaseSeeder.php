8886b9f0e38512c986dcf9242749e830422c3958
codesultan <codesultan369@gmail.com>
2025-01-02 21:02:34 +0100

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-
+        User::create(
+            [
+                'name' => 'Olusegun Ibraheem',
+                'email' => 'codesultan369@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => 'NpkjO0rOQ5b'
+            ]
+        );
         $channels = collect(['general', 'laravel', 'reverb', 'echo', 'livewire']);
+
 
         $channels->each(function ($channel) {
             Channel::firstOrCreate(['name' => $channel])
